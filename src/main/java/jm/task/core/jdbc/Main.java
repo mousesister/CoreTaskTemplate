b09941e92d92e26e1a7a950d7094de6bdcd5bcda
mousesister <mousesister@mail.ru>
2021-07-19 20:47:24 +0300

    не проходит тесты

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,45 @@
 package jm.task.core.jdbc;
 
+
+import jm.task.core.jdbc.util.Util;
+import java.sql.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        Util util = new Util();
+        Statement statement;
+        try {
+            statement = util.getConnection().createStatement();
+            String createTable = "CREATE TABLE IF NOT EXISTS User(id int not null primary key AUTO_INCREMENT, " +
+                    "name varchar(50) NOT NULL default 'Jone', lastName varchar(40) NOT NULL default 'Dow', " +
+                    "age tinyint NOT NULL default 25)";
+            statement.execute(createTable);
+            statement.executeUpdate("INSERT INTO User(name, lastName, age) VALUES ('Jone', 'Dow', 42), " +
+                                                "('Jane', 'Dow', 51), ('Thomas', 'Hanks', 65), ('Rita', 'Hayworth', 103) ");
+
+            ResultSet rs = statement.executeQuery("SELECT * FROM User");
+            while (rs.next()) {
+                String name = rs.getString("name");
+                System.out.println("User with name: " + name + " has been added to the database");
+            }
+            statement.executeUpdate("TRUNCATE TABLE User");
+            statement.executeUpdate("DROP TABLE IF EXISTS User");
+            util.getConnection().close();
+            if (util.getConnection().isClosed()) {
+                System.out.println("Соединение с базой закрыто");
+            }
+
+
+        } catch(SQLException se){
+                System.out.println("что-то пошло не так");
+                se.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,41 @@
 package jm.task.core.jdbc.util;
 
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    Connection conn;
+
+    public Connection getConnection() {
+        return conn;
+    }
+
+    public Util()  {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection
+                    ("jdbc:mysql://localhost:3306/test_one",
+                            "admin", "POI456ert");
+            //conn.setAutoCommit(false);
+            if (!conn.isClosed()) {
+                System.out.println("соединение с БД установлено!");
+            }
+
+
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+            System.out.println("JDBC драйвер для СУБД не найден!");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Ошибка SQL !");
+
+        }
+    }
+
+
 }
